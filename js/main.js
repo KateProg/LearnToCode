eb5b8be26f043fb3dbd4f1e6ee76f3d273df37b2
KateProg <kate.healy@outlook.com>
2022-10-19 16:53:44 +1100

    Changes to be committed: 	modified:   css.html     modified:   index.html 	modified:   java.html 	modified:   js/main.js 	modified:   learn.html 	modified:   useful.html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let codeIcon = "Hello world!";
+console.log(codeIcon);
+
+function message() {
+    alert('nothing happened... Just pretend a page turned lol');
+}
+
+function cursor() {
+    let cursor = document.querySelector('#book');
+    cursor.style.cursor = "pointer"
+}
